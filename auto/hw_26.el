eeb5505a12ff308b121505f36087cf86c4042f92
Cris Forno <cforno1@binghamton.edu>
2017-11-29 20:50:33 -0500

    finished HW#26

---
+++ auto/hw_26.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook
+ "hw_26"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "amssymb"))
+ :latex)
+
